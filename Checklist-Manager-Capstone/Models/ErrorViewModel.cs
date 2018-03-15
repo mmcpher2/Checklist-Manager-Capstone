fb927b665d3e380ad069d7ae0565ffc82a405cb1
Ryan McPherson <mmcpher2@gmail.com>
2018-03-15 09:42:45 -0700

    Fixing setup

---
+++ Checklist-Manager-Capstone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NursingChecklistManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
